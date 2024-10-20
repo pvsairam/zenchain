479bda2dce4feec41297922bdb39c639010e2695
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2024-10-20 10:54:04 +0530

    Update test.py

--- stake/test.py
+++ stake/test.py
@@ -83,15 +83,14 @@ NATIVE_STAKING_ABI = [
 
 staking_contract = w3.eth.contract(address=NATIVE_STAKING_ADDRESS, abi=NATIVE_STAKING_ABI)
 
-
+chain_id = 8408
 
 def send_transaction(func):
-    nonce = w3.eth.get_transaction_count(MY_ADDRESS)
     transaction = func.build_transaction({
-        'chainId': 8408,
+        'chainId': chain_id,
         'gas': 2000000,
         'gasPrice': w3.eth.gas_price,
-        'nonce': nonce,
+        'nonce': w3.eth.get_transaction_count(MY_ADDRESS),
     })
     
     signed_txn = w3.eth.account.sign_transaction(transaction, private_key=PRIVATE_KEY)
