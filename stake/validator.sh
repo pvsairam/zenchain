cea7a63665f6545436e670601a1ad6b7b7b97dc4
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2024-10-21 17:19:26 +0530

    Update validator.sh

--- stake/validator.sh
+++ stake/validator.sh
@@ -248,7 +248,6 @@ refresh_node() {
     docker run \
     -d \
     --name zenchain \
-    -p 30333:30333 \
     -p 9944:9944 \
     -v "$HOME/chain-data:/chain-data" \
     ghcr.io/zenchain-protocol/zenchain-testnet:latest \
